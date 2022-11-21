28882a4137f5cfe0c3cd0f816844605c477f7ddd
liuyi3 <liuyi3@foundersc.com>
2022-11-21 13:35:25 +0800

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,18 @@
 module.exports = {
   pathPrefix: '/running_page', // Change to `/running_page` when running on github pages
   siteMetadata: {
-    siteTitle: 'Running Page',
-    siteUrl: 'https://yihong.run',
+    siteTitle: 'mW Running Page',
+    siteUrl: 'https://www.strava.com/athletes/110789274',
     logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
     description: 'Personal site and blog',
     navLinks: [
       {
         name: 'Blog',
-        url: 'https://yihong.run/running',
+        url: 'https://www.strava.com/athletes/110789274',
       },
       {
         name: 'About',
-        url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+        url: 'https://www.strava.com/athletes/110789274',
       },
     ],
   },
